c2467f5e2796100bb6b289a4633d2e5bfab7e41e
Gabriel Cavalheiro Barreto <gabrielcavalheiro@locus.software>
2022-05-16 00:37:11 -0300

    feat: add test refresh token sucessful

--- bidding/authentication/tests/views/test_user_auth_refresh_api_view.py
+++ bidding/authentication/tests/views/test_user_auth_refresh_api_view.py
@@ -4,6 +4,7 @@ from faker import Faker
 from django.urls import reverse
 from rest_framework.test import APIClient
 from django.contrib.auth.models import User
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 
 class TestUserAuthAPIView:
@@ -24,16 +25,24 @@ class TestUserAuthAPIView:
         assert response.status_code == 401
         assert response.json() == authentication_refresh_user_invalid_credentials
 
-    # @pytest.mark.django_db
-    # def test_should_return_valid_refresh(self):
-    #     password = self.fake.pystr()
-    #     user = User.objects.create_user(username=self.fake.name(), password=password)
-    #     data = {
-    #         'username': user.username,
-    #         'password': password
-    #     }
+    @pytest.mark.django_db
+    def test_should_return_valid_refresh(self):
+        password = self.fake.pystr()
+        user = User.objects.create_user(username=self.fake.name(), password=password)
+        user_data = {
+            'username': user.username,
+            'password': password
+        }
 
-    #     response = self.api_client.post(self.url, data=data)
+        serializer = TokenObtainPairSerializer(data=user_data)
+        serializer.is_valid(raise_exception=True)
 
-    #     assert response.status_code == 200
-    #     assert 'access' in response.json()
+        refresh = serializer.validated_data['refresh']
+        data = {
+            'refresh': refresh
+        }
+
+        response = self.api_client.post(self.url, data=data)
+
+        assert response.status_code == 200
+        assert 'access' in response.json()
