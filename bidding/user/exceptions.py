a9744f5096d1d6ea234f1c305a162712b59a9e85
Gabriel Cavalheiro <ga3-cavalheiro@hotmail.com>
2022-05-25 23:39:56 -0300

    fix: method init name

--- bidding/user/exceptions.py
+++ bidding/user/exceptions.py
@@ -6,11 +6,11 @@ class UserNotFound(ObjectNotFound):
     DEFAULT_CODE = 'USER_NOT_FOUND'
     DEFAULT_STATUS_CODE = 404
 
-    def _init_(self,
+    def __init__(self,
                message: str = DEFAULT_MESSAGE,
                code: str = DEFAULT_CODE,
                status_code: int = DEFAULT_STATUS_CODE):
-        super()._init_(message=message, code=code, status_code=status_code)
+        super().__init__(message=message, code=code, status_code=status_code)
 
 
 class UserAlreadyExists(BiddingValidationError):
